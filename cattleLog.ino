e340ee472820fa43c1630e8ca9195226a7768382
toasthall <toasthall@gmail.com>
2016-12-19 09:27:34 +0930

    Update cattleLog.ino

--- cattleLog.ino
+++ cattleLog.ino
@@ -1,101 +1,88 @@
-#include <Time.h>
-#include <TimeLib.h>
 #include <TinyGPS.h>
 #include <SoftwareSerial.h>
-#include <Average.h>
+#define SD_CS_PIN SS
+#include <SPI.h>
+#include "SdFat.h"
+SdFat SD;
 
 
+SoftwareSerial ss(5, 6); //RX, TX for dev only
+File myFile;
 TinyGPS gps;
-SoftwareSerial ss(10, 11);
 
-
-void setup() 
+void setup()
 {
+  // Open serial communications and wait for port to open:
   Serial.begin(115200);
-  ss.begin(9600);
+    //Wait for USB Serial 
+    while (!Serial) 
+  {
+    SysCall::yield();
+  }
+    ss.begin(9600);
+
+
+  Serial.print("Initializing SD card...");
+  pinMode(10, OUTPUT);
+
+  if (!SD.begin(SD_CS_PIN)) 
+  {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
 }
 
-void loop() 
+void loop()
 {
-  char unitID[] = "TEST01"; //unique ID for animal eg ear tag
   bool newData = false;
-  float flat, flong;
-  float offset = 9.5;
-  unsigned long age;
   unsigned long chars;
   unsigned short sentences, failed;
-  Average <float> flatStore(29);
-  Average <float> flongStore(29);
-  int Year;
-  byte Month, Day, Hour, Minute, Second;
-
   
-  //for (unsigned long start = millis(); millis() - start < 1000;)
-  for (int x = 1; x <29; x++) //30 readings
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  for (unsigned long start = millis(); millis() - start < 1000;)
   {
-    for (unsigned long start = millis(); millis() - start < 1000;)
+    while (ss.available())
     {
-      while (ss.available())
+      char c = ss.read();
+      if (gps.encode(c))
+      newData = true;
+    }
+  }
+   if (newData)
+   {
+    myFile = SD.open("log.txt", FILE_WRITE);
+
+    // if the file opened okay, write to it:
+    if (myFile) 
       {
-        char c = ss.read();
-        //Serial.write(c);
-        if (gps.encode(c))
-        newData = true;
+        float flat, flon;
+        unsigned long age;
+        gps.f_get_position(&flat, &flon, &age);
+        myFile.print("LAT=");
+        myFile.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+        myFile.print(" LON=");
+        myFile.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+        myFile.print(" SAT=");
+        myFile.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+        myFile.print(" PREC=");
+        myFile.println(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+        // flash write led
+        myFile.close(); //close the file
+      } 
+      else 
+      {
+        // ERROR LED
       }
     }
-      
-      if (newData)
-      {  
-        gps.f_get_position(&flat, &flong, &age);
-        gps.crack_datetime(&Year, &Month, &Day, &Hour, &Minute, &Second, NULL, NULL);
-        setTime(Hour, Minute, Second, Day, Month, Year);
-        adjustTime(offset * SECS_PER_HOUR);
-        digitalClockDisplay();
-        Serial.print(unitID);
-        Serial.print(", ");
-        Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-        Serial.print(", ");
-        Serial.print(flong == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flong, 6);
-        Serial.print(", ");
-        Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
-        Serial.print(", ");
-        Serial.println(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
-      }
-
-     else
-     {
-      gps.stats(&chars, &sentences, &failed);
-      Serial.print(" CHARS=");
-      Serial.print(chars);
-      Serial.print(" SENTENCES=");
-      Serial.print(sentences);
-      Serial.print(" CSUM ERR=");
-      Serial.println(failed);
-      if (chars == 0)
-      Serial.println("** No characters received from GPS: check wiring **");
-     }
-  }
-  
-  Serial.println("wait 10 seconds");
-  delay(10000);
-}
-
-void digitalClockDisplay(){
-  // digital clock display of the time
-  Serial.print(hour());
-  printDigits(minute());
-  printDigits(second());
-  Serial.print(", ");
-  Serial.print(day());
-  Serial.print(month());
-  Serial.print(year()); 
-  Serial.print(", "); 
-}
-
-void printDigits(int digits) {
-  // utility function for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
+  gps.stats(&chars, &sentences, &failed);
+  Serial.print(" CHARS=");
+  Serial.print(chars);
+  Serial.print(" SENTENCES=");
+  Serial.print(sentences);
+  Serial.print(" CSUM ERR=");
+  Serial.println(failed);
+  if (chars == 0)
+    Serial.println("** No characters received from GPS: check wiring **");
+} 
